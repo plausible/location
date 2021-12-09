6c7a372eed1065020f3d379723d2417784b1c41b
Uku Taht <uku.taht@gmail.com>
2021-12-09 16:00:01 +0200

    Add debug logs

--- lib/location.ex
+++ lib/location.ex
@@ -1,4 +1,5 @@
 defmodule Location do
+  require Logger
   defdelegate get_country(alpha_2), to: Location.Country
   defdelegate get_subdivision(code), to: Location.Subdivision
   defdelegate get_city(code), to: Location.City
@@ -6,10 +7,14 @@ defmodule Location do
   def load_all do
     me = self()
 
+    Logger.debug("Loading location databases...")
+
     [
       Task.async(fn -> Location.Country.load(me) end),
       Task.async(fn -> Location.Subdivision.load(me) end),
       Task.async(fn -> Location.City.load(me) end)
     ] |> Enum.map(fn task -> Task.await(task, 15_000) end)
+
+    Logger.debug("Location databases loaded")
   end
 end
