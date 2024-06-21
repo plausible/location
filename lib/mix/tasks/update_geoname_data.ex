a014fc525a3b48bdc5c25ea396156d6c9c2ae859
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-06-21 15:26:45 +0700

    mix format

--- lib/mix/tasks/update_geoname_data.ex
+++ lib/mix/tasks/update_geoname_data.ex
@@ -8,22 +8,24 @@ defmodule Mix.Tasks.UpdateGeonameData do
   The data source allCountries.txt clocks in at 1.5GB. Expect this to take a while.
   """
   def run(_) do
-    #System.cmd("wget", [@allcountries_src, "-O", "/tmp/allCountries.zip"])
-    #System.cmd("unzip", ["/tmp/allCountries.zip", "-d", "/tmp"])
+    # System.cmd("wget", [@allcountries_src, "-O", "/tmp/allCountries.zip"])
+    # System.cmd("unzip", ["/tmp/allCountries.zip", "-d", "/tmp"])
 
     process_geonames_file("/tmp/allCountries.txt")
   end
 
   defp process_geonames_file(filename) do
-    tab = :binary.compile_pattern("\t") # BINARY
+    # BINARY
+    tab = :binary.compile_pattern("\t")
 
-    result = filename
-    |> File.stream!(read_ahead: 100_000)
-    |> Flow.from_enumerable()
-    |> Flow.map(&String.split(&1, tab))
-    |> Flow.partition()
-    |> Flow.reduce(fn -> [] end, &reduce_chunk/2)
-    |> Enum.into([])
+    result =
+      filename
+      |> File.stream!(read_ahead: 100_000)
+      |> Flow.from_enumerable()
+      |> Flow.map(&String.split(&1, tab))
+      |> Flow.partition()
+      |> Flow.reduce(fn -> [] end, &reduce_chunk/2)
+      |> Enum.into([])
 
     IO.puts("Writing result to #{@allcountries_dest}")
 
@@ -32,10 +34,14 @@ defmodule Mix.Tasks.UpdateGeonameData do
 
   defp reduce_chunk(row, result) do
     case row do
-      [geoname_id, name, _, _, _, _, feature_class, _, country_code | _rest] when feature_class in ["P", "A"] -> # feature classes defined here: http://download.geonames.org/export/dump/
+      # feature classes defined here: http://download.geonames.org/export/dump/
+      [geoname_id, name, _, _, _, _, feature_class, _, country_code | _rest]
+      when feature_class in ["P", "A"] ->
         row = geoname_id <> "\t" <> name <> "\t" <> country_code
         [row | result]
-      _ -> result
+
+      _ ->
+        result
     end
   end
 end
