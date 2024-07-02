080ce76e5e6cb0976c39010f204871025b349c8e
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-07-02 19:20:12 +0700

    wip

--- mix_tasks/scraper.ex
+++ mix_tasks/scraper.ex
@@ -1,12 +1,15 @@
 defmodule Location.Scraper do
-  @base_url "https://en.wikipedia.org"
-  @subdivision_base_url @base_url <> "/wiki/ISO_3166-2:"
+  @base_url "https://en.wikipedia.org/wiki/ISO_3166-2:"
   @translations_dest Application.app_dir(:location, "/priv/iso_3166-2.en-translations.json")
   @countries_to_skip [
     # For Estonia the local names are better than English ones
     "EE",
     # Source data from salsa-debian already has english translations where applicable
-    "JP"
+    "JP",
+    "MW",
+    "FJ",
+    "GH",
+    "UG"
   ]
 
   def scrape() do
@@ -16,7 +19,6 @@ defmodule Location.Scraper do
       Enum.map(countries, &scrape_country/1)
       |> Enum.filter(&(not is_nil(&1)))
       |> List.flatten()
-      |> Enum.sort()
       |> Jason.OrderedObject.new()
       |> Jason.encode_to_iodata!(pretty: true)
 
@@ -26,47 +28,45 @@ defmodule Location.Scraper do
   defp scrape_country(%Location.Country{alpha_2: code}) when code in @countries_to_skip, do: nil
 
   defp scrape_country(country) do
-    url = @subdivision_base_url <> country.alpha_2
+    url = @base_url <> country.alpha_2
     response = HTTPoison.get!(url)
     {:ok, document} = Floki.parse_document(response.body)
 
     rows =
-      Floki.find(document, "table.wikitable.sortable")
-      |> List.first()
-      |> then(fn table ->
-        if table do
-          Floki.find(table, "tbody tr")
+      document
+      |> Floki.find("table.wikitable.sortable")
+      |> Enum.flat_map(fn table ->
+        rows = Floki.find(table, "tbody tr")
+
+        english_name_column =
+          case List.first(rows) do
+            {"tr", _attrs, cells} ->
+              Enum.find_index(cells, fn cell ->
+                text = String.downcase(cell_text(cell))
+                # https://github.com/plausible/analytics/issues/3260
+                if country.alpha_2 == "SE" do
+                  String.starts_with?(text, "subdivision name (sv)")
+                else
+                  String.starts_with?(text, "subdivision name (en)")
+                end
+              end)
+
+            _ ->
+              nil
+          end
+
+        if english_name_column do
+          IO.puts(IO.ANSI.green() <> "Scraping " <> country.name <> IO.ANSI.reset())
+
+          Enum.drop(rows, 1)
+          |> Enum.map(fn row -> scrape_row(row, english_name_column) end)
+          |> Enum.uniq_by(fn {code, _name} -> code end)
+          |> Enum.sort_by(fn {code, _name} -> code end)
         else
+          IO.puts(IO.ANSI.red() <> "Skipping " <> country.name <> IO.ANSI.reset())
           []
         end
       end)
-
-    english_name_column =
-      case List.first(rows) do
-        {"tr", _attrs, cells} ->
-          Enum.find_index(cells, fn cell ->
-            text = String.downcase(cell_text(cell))
-            # https://github.com/plausible/analytics/issues/3260
-            if country.alpha_2 == "SE" do
-              String.starts_with?(text, "subdivision name (sv)")
-            else
-              String.starts_with?(text, "subdivision name (en)")
-            end
-          end)
-
-        _ ->
-          nil
-      end
-
-    if english_name_column do
-      IO.puts("Scraping " <> country.name)
-
-      Enum.drop(rows, 1)
-      |> Enum.map(fn row -> scrape_row(row, english_name_column) end)
-    else
-      IO.puts("Skipping " <> country.name)
-      []
-    end
   end
 
   defp scrape_row({"tr", _attrs, children}, name_column_index) do
