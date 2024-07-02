ff2f7fe3c9663378b83ecbe9e03fede2f3b9125f
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-07-02 15:21:10 +0700

    update deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Location.MixProject do
     [
       {:jason, "~> 1.3"},
       {:nimble_csv, "~> 1.1"},
-      {:floki, "~> 0.31.0", only: [:dev, :test]},
+      {:floki, "~> 0.36.0", only: [:dev, :test]},
       {:httpoison, "~> 1.8", only: [:dev, :test]},
       {:flow, "~> 1.0", only: [:dev, :test]}
     ]
