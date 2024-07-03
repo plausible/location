3c924623deb5d35f5a061f324f941cbc7bd4ab2b
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-07-03 11:02:26 +0700

    allow old elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Location.MixProject do
     [
       app: :location,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       description: description(),
