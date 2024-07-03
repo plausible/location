a594b7dda63295194c0ce59ee38327b712fc8566
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-07-03 12:45:28 +0700

    update_english_translations

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Elixir library for accessing ISO3166-1 (country) and ISO3166-2 (subdivision) data as well as geoname data for cities. Source data comes from the upstream [debian iso-codes](https://salsa.debian.org/iso-codes-team/iso-codes) package and the [Geonames](http://www.geonames.org/) project.
 
-
-
 ### Countries
 
 The data for countries comes primarily from the [debian iso-codes](https://salsa.debian.org/iso-codes-team/iso-codes) package. The data file for that is stored in `priv/iso_3166-1.json`. We do
@@ -11,10 +9,7 @@ manually add some data that is missing from upstream. Overrides can be found in 
 
 ### Subdivisions
 
-The data for subdivisions comes primarily from the [debian iso-codes](https://salsa.debian.org/iso-codes-team/iso-codes) package. The data file for that is stored in `priv/iso_3166-2.json`. The
-subdivision names in this file are mostly in local language (i.e. Wien instead of Vienna). English translations are obtained from Wikipedia using a scraper. The translations found in `priv/iso_3166-2.en-translations.json` are used when available instead of the original name.
-
-We also add some data manually that is missing from upstream. Overrides can be found in `priv/override/iso_3166-2.json`
+The data for subdivisions comes primarily from the [debian iso-codes](https://salsa.debian.org/iso-codes-team/iso-codes) package. The data file for that is stored in `priv/iso_3166-2.json`. The subdivision names in this file are mostly in local language (i.e. Wien instead of Vienna). English translations are obtained from CLDR and Wikipedia. The translations found in `priv/iso_3166-2.en-translations.json` are used when available instead of the original name.
 
 ### Cities
 
--- test/location_test.exs
+++ test/location_test.exs
@@ -50,7 +50,7 @@ defmodule LocationTest do
       assert Location.get_subdivision("GT-AV").name == "Alta Verapaz"
 
       # IN-CT and some other codes were changed in 2023-11-23: https://en.wikipedia.org/wiki/ISO_3166-2:IN#Changes
-      assert Location.get_subdivision("IN-CT").name == "Chhattīsgarh"
+      assert Location.get_subdivision("IN-CT").name == "Chhattisgarh"
 
       # IS-BFJ and some other codes were removed in 2021-11-25: https://en.wikipedia.org/wiki/ISO_3166-2:IS#Changes
       assert Location.get_subdivision("IS-BFJ").name == "Borgarfjarðarhreppur"
@@ -62,13 +62,78 @@ defmodule LocationTest do
       assert Location.get_subdivision("KZ-ALA").name == "Almaty"
 
       # LV-001 and some other codes were removed in 2021-11-25: https://en.wikipedia.org/wiki/ISO_3166-2:LV#Changes
-      assert Location.get_subdivision("LV-001").name == "Aglonas novads"
+      assert Location.get_subdivision("LV-001").name == "Aglona"
 
       # NP-1 and some other codes were removed in 2022-11-29: https://en.wikipedia.org/wiki/ISO_3166-2:NP#Changes
       assert Location.get_subdivision("NP-1").name == "Central"
 
       # PH-MAG was split in two in 2023-11-23: https://en.wikipedia.org/wiki/ISO_3166-2:PH#Changes
       assert Location.get_subdivision("PH-MAG").name == "Maguindanao"
+    end
+
+    # # https://unicode-org.atlassian.net/browse/CLDR-17777
+    test "can translate new subdivisions" do
+      assert Location.get_subdivision("ID-PD").name == "Southwest Papua"
+      assert Location.get_subdivision("ID-PE").name == "Highland Papua"
+      assert Location.get_subdivision("ID-PS").name == "South Papua"
+      assert Location.get_subdivision("ID-PT").name == "Central Papua"
+
+      assert Location.get_subdivision("IS-HUG").name == "Huna Settlement"
+      assert Location.get_subdivision("IS-SKR").name == "Skagafjordur"
+
+      assert Location.get_subdivision("KP-15").name == "Kaesong"
+
+      assert Location.get_subdivision("KZ-10").name == "Abai"
+      assert Location.get_subdivision("KZ-11").name == "Akmola"
+      assert Location.get_subdivision("KZ-15").name == "Aktobe"
+      assert Location.get_subdivision("KZ-19").name == "Almaty"
+      assert Location.get_subdivision("KZ-23").name == "Atyrau"
+      assert Location.get_subdivision("KZ-27").name == "West Kazakhstan"
+      assert Location.get_subdivision("KZ-31").name == "Jambyl"
+      assert Location.get_subdivision("KZ-33").name == "Jetisu"
+      assert Location.get_subdivision("KZ-35").name == "Karaganda"
+      assert Location.get_subdivision("KZ-39").name == "Kostanay"
+      assert Location.get_subdivision("KZ-43").name == "Kyzylorda"
+      assert Location.get_subdivision("KZ-47").name == "Mangystau"
+      assert Location.get_subdivision("KZ-55").name == "Pavlodar"
+      assert Location.get_subdivision("KZ-59").name == "North Kazakhstan"
+      assert Location.get_subdivision("KZ-61").name == "Turkistan"
+      assert Location.get_subdivision("KZ-62").name == "Ulytau"
+      assert Location.get_subdivision("KZ-63").name == "East Kazakhstan"
+      assert Location.get_subdivision("KZ-75").name == "Almaty City"
+
+      assert Location.get_subdivision("FI-01").name == "Åland"
+
+      assert Location.get_subdivision("FR-BL").name == "Saint Barts"
+      assert Location.get_subdivision("FR-NC").name == "New Caledonia"
+      assert Location.get_subdivision("FR-PF").name == "French Polynesia"
+      assert Location.get_subdivision("FR-PM").name == "Saint Peter and Miquelon"
+      assert Location.get_subdivision("FR-TF").name == "French Southern Territories"
+      assert Location.get_subdivision("FR-WF").name == "Wallis and Futuna"
+    end
+
+    test "can translate" do
+      # JP
+      assert Location.get_subdivision("JP-01").name == "Hokkaido"
+      assert Location.get_subdivision("JP-13").name == "Tokyo"
+      assert Location.get_subdivision("JP-21").name == "Gifu"
+
+      # MW
+      assert Location.get_subdivision("MW-C").name == "Central"
+      assert Location.get_subdivision("MW-BA").name == "Balaka"
+
+      # FJ
+      assert Location.get_subdivision("FJ-03").name == "Cakaudrove"
+      assert Location.get_subdivision("FJ-08").name == "Nadroga-Navosa"
+      assert Location.get_subdivision("FJ-C").name == "Central"
+
+      # GH
+      assert Location.get_subdivision("GH-AA").name == "Greater Accra"
+      assert Location.get_subdivision("GH-CP").name == "Central"
+
+      # CZ
+      assert Location.get_subdivision("CZ-10").name == "Prague"
+      assert Location.get_subdivision("CZ-41").name == "Karlovy Vary"
     end
   end
 
